b75f5800d17a256c7407ec4f656c1f3bd636e4f7
Emre Aslan <52652572+nitrozeus0x01@users.noreply.github.com>
2020-04-26 14:57:43 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,12 +1,36 @@
 import requests
-
-print("""
-# WEB SITE STATUS CODE CHECKER #
-	""")
-
-site = input("Enter a site for checking : ")
-print("")
-site1 = requests.get(site)
-print("Status Code Checked : ",site1.status_code)
-print("Elapsed Time : ",site1.elapsed.total_seconds())
-print("")
+print""
+site = raw_input("[ ? ]  Enter Site : ")
+print ""
+reGet = requests.get(site)
+rePost = requests.post(site)
+rePut = requests.put(site)
+reDel = requests.delete(site)
+print "--------------------- Get Method Checked --------------------------- "
+print "[ + ] Site : ",site
+print "[ + ] Status Code : ",reGet.status_code
+print "[ + ] Requestable : ",reGet.ok
+print "[ + ] Timing : ",reGet.elapsed.total_seconds()
+print "---------------------------------------------------------------------------"
+print ""
+print "-------------------- Post Method Checked --------------------------- "
+print "[ + ] Site : ",site
+print "[ + ] Status Code : ",rePost.status_code
+print "[ + ] Requestable : ",rePost.ok
+print "[ + ] Timing : ",rePost.elapsed.total_seconds()
+print "---------------------------------------------------------------------------"
+print ""
+print "--------------------- Put Method Checked --------------------------- "
+print "[ + ] Site : ",site
+print "[ + ] Status Code : ",rePut.status_code
+print "[ + ] Requestable : ",rePut.ok
+print "[ + ] Timing : ",rePut.elapsed.total_seconds()
+print "---------------------------------------------------------------------------"
+print ""
+print "------------------- Delete Method Checked -------------------------- "
+print "[ + ] Site : ",site
+print "[ + ] Status Code : ",reDel.status_code
+print "[ + ] Requestable : ",reDel.ok
+print "[ + ] Timing : ",reDel.elapsed.total_seconds()
+print "---------------------------------------------------------------------------"
+print ""
